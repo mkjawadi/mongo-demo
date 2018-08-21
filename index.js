607e354fb7ce908c8a2a413ed60c5bef6e61edc7
Kumail Jawadi <mk.jawadi@hotmail.com>
2018-08-21 14:21:18 +0430

    implement mongoose validation (built-in, custom, and async validators).

--- index.js
+++ index.js
@@ -5,26 +5,64 @@ mongoose.connect('mongodb://localhost/mongo-demo')
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
 const courseSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String, 
+    required: true,
+    minlength: 5,
+    maxlength: 255
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ['web', 'mobile', 'network'],
+    lowercase: true
+  },
   author: String, 
-  tags: [ String ],
+  tags: {
+    type: Array,
+    validate: {
+      isAsync: true,
+      validator: function(v, callback) {
+        setTimeout(() => {
+          const result = v && v.length > 0;
+          callback(result);
+        }, 4000);
+    },
+    message: 'A course should have at least one tag.'
+    }
+  },
   date: { type: Date, default: Date.now}, 
   isPublished: Boolean,
-  price: Number
+  price: {
+    type: Number,
+    required: function(){ return this.isPublished },
+    min: 10,
+    max:200
+    get: v=> Math.round(v),
+    set: v=> Math.round(v),
+  }
 });
 
 const Course = mongoose.model('Course', courseSchema);
 
 async function createCourse() {
   const course= new Course({
-    name: 'Angular Course',
-    author: 'Kumail Jawadi',
-    tags: ['angular', 'frontend'],
-    isPublished: true
+    name: 'PHP Laravel Course',
+    category: 'web',
+    author: 'Belal',
+    tags: ['php','laravel', 'backend'],
+    isPublished: true,
+    price: 100,
   });
 
+try{
 const result = await course.save();
 console.log(result);
+}
+catch(ex){
+  for(field in ex.errors)
+    console.log(ex.errors[field].message);
+}
 }
 
 // createCourse();
@@ -80,6 +118,8 @@ async function removeCourse(id){
   const course = await Course.findByIdAndRemove(id);
   console.log(course);
 }
+
+createCourse();
 // getCourses();
 // updateCourse('5b67ea3f7944e92db0e2b565');
-removeCourse('5b67ea3f7944e92db0e2b565');
+// removeCourse('5b67ea3f7944e92db0e2b565');
